7cf11be9ea2f28da23849c498b9e9595b4cbb47d
PedroNCordeiro <pncordeiro@gmail.com>
2017-11-12 14:51:09 +0100

    Fix ball collision on a paddle
    To avoid weird reflections, an intersection area is calculated to tell which part of the paddle the ball hit

--- gameManager.js
+++ gameManager.js
@@ -1,13 +1,49 @@
+// Let's determine which surface of the paddle the ball collided with
+// Here we suppose the hit happened where the ball has a lower intersection area
+function ReflectBallOnPaddle(currentAngle, intersection) {
+	var horizontalColisionArea = Math.min(Math.abs(intersection.right), 
+		Math.abs(intersection.left));
+
+	var verticalColisionArea = Math.min(Math.abs(intersection.top), 
+		Math.abs(intersection.bottom));
+
+	// Vertical surface reflection
+	if (horizontalColisionArea <= verticalColisionArea) {
+		return Math.PI - currentAngle;
+	}
+
+	// Horizontal surface reflection
+	return currentAngle *= -1;
+}
+
 function collisionBallPaddles(ball, paddles) {
 	for (var i = 0; i < paddles.length; i++) {
-		if (ball.position.x + ball.size*.5 > paddles[i].x - paddles[i].thickness*.5 &&
-			ball.position.x - ball.size*.5 < paddles[i].x + paddles[i].thickness*.5 &&
-			ball.position.y - ball.size*.5 < paddles[i].y + paddles[i].length*.5 &&
-			ball.position.y + ball.size*.5 > paddles[i].y - paddles[i].length*.5) {
-			
-			// Vertical surface reflection
-			ball.angle = Math.PI - ball.angle;
+
+		var rBallIntersect = ball.position.x + ball.size*.5 - (paddles[i].x - paddles[i].thickness*.5);
+		if (rBallIntersect <= 0) {
+			continue;
 		}
+		var lBallIntersect = ball.position.x - ball.size*.5 - (paddles[i].x + paddles[i].thickness*.5);
+		if (lBallIntersect >= 0) {
+			continue;
+		}
+		var tBallIntersect = ball.position.y - ball.size*.5 - (paddles[i].y + paddles[i].length*.5);
+		if (tBallIntersect >= 0) {
+			continue;
+		}
+		var bBallIntersect = ball.position.y + ball.size*.5 - (paddles[i].y - paddles[i].length*.5);
+		if (bBallIntersect <= 0) {
+			continue;
+		}
+		
+		// Arriving here means the ball collided with a paddle
+		ball.angle = ReflectBallOnPaddle(ball.angle, 
+			{right: rBallIntersect,
+			left: lBallIntersect,
+			top: tBallIntersect,
+			bottom: bBallIntersect
+		});
+
 	}
 }
 
